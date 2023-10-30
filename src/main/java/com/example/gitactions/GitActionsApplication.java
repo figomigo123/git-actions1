6b890a5062545f77fb3bc99f33ae166a68aa423c
farag <faragmikhail29@gmail.com>
2023-10-30 19:52:30 +0200

    Initial commit

---
+++ src/main/java/com/example/gitactions/GitActionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitactions/GitActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
